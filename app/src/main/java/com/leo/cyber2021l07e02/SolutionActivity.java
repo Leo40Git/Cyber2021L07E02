6c2ec98fea0dc28e9140151128a291e06292d84e
Kfir Awad <7997354+Leo40Git@users.noreply.github.com>
2019-05-30 09:06:57 +0300

    design

--- app/src/main/java/com/leo/cyber2021l07e02/SolutionActivity.java
+++ app/src/main/java/com/leo/cyber2021l07e02/SolutionActivity.java
@@ -4,20 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class SolutionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_solution);
     }
 
-    public void onClick_Clear(View view) {
-    }
-
-    public void onClick_Random(View view) {
-    }
-
-    public void onClick_Solve(View view) {
+    public void onClick_Back(View view) {
     }
 }
